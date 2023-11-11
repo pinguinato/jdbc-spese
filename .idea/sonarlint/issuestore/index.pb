95fc8a01400ed524d6e7c48f894a9e62b7da915d
Roberto Gianotto <gianottoroberto@gmail.com>
2023-11-11 14:30:30 +0100

    completamento dei test di validazione di un oggetto Spesa

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-7
-pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-Y
-)src/main/java/it/gianotto/spese/Main.java,d/1/d1b7ce63a67cc329bb8c8420a412feea0a138a39
-`
-0src/main/java/it/gianotto/spese/model/Spesa.java,9/1/91c7e1d1ec09bab5da4fed6ffd314017dfcc3935
--- src/test/java/it/gianotto/spese/model/SpesaTest.java
+++ src/test/java/it/gianotto/spese/model/SpesaTest.java
@@ -1,8 +1,7 @@
 package it.gianotto.spese.model;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SpesaTest {
 
@@ -19,6 +18,39 @@ class SpesaTest {
         assertNotNull(spesa);
         assertEquals(Spesa.class, spesa.getClass());
         System.out.println(spesa);
+    }
+
+    @Test
+    void invalidIdTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(null, "Test spesa", "test di una spesa", "Roberto Gianotto", 134.25);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(-1, "Test spesa", "test di una spesa", "Roberto Gianotto", 134.25);
+        });
+    }
+
+    @Test
+    void invalidTitoloTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(1111, null, "test di una spesa", "Roberto Gianotto", 134.25);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(1111, "", "test di una spesa", "Roberto Gianotto", 134.25);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(1111, "aa", "test di una spesa", "Roberto Gianotto", 134.25);
+        });
+    }
+
+    @Test
+    void invalidTotaleSpesaTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(1111, "test spesa", "test di una spesa", "Roberto Gianotto", null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Spesa(1111, "test spesa", "test di una spesa", "Roberto Gianotto", -10.01);
+        });
     }
 
 }
